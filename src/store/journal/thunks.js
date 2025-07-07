59c45a1e6e93d510194a6df6b717961a4ad902b3
Lucas <lucas.82@hotmail.com.ar>
2025-07-07 12:46:26 -0300

    version 1.0

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -22,8 +22,8 @@ export const startNewNote = () => {
     const newNote = {
       title: "",
       body: "",
-      date: new Date().getTime(),
       imageUrls: [],
+      date: new Date().getTime(),
     };
 
     const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
